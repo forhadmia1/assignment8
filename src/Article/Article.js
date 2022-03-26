5a9b7cf82748a059fd7847b6ed567ac28b5cdd4f
forhadmia1 <forhad8613@gmail.com>
2022-03-26 18:10:08 +0600

    question answer added and style them

--- src/Article/Article.js
+++ src/Article/Article.js
@@ -4,12 +4,18 @@ import './Article.css'
 const Article = () => {
     return (
         <div>
+            <div className='top-blog-container'>
+                <article className='blog-container'>
+                    <h1>1. How React Works?</h1>
+                    <p>React is a javascript library, use to create the user interface. React combined the data and when rendering into the browser create a virtual dom copy. When data is changed react compare the previous virtual dom copy and new virtual dom copy and create a new virtual dom copy with updated data and only updates dom where necessary.</p>
+                </article>
+                <article className='blog-container'>
+                    <h1>2. How useState Works?</h1>
+                    <p>UseState is a react hook, that contains a variable and a function. At the first time when a component loads usestate set the initial value to its variable. When changing the state then the setState function assign and update the new value to the usestate variable and finally rerender the function.</p>
+                </article>
+            </div>
             <article className='blog-container'>
-                <h1>1. How React Works?</h1>
-                <p>React is a javascript libries, use to create user interface. React combined the data and wehen rendering into browser create a virtual dom copy. When a data is changed react compare the previous virtual dom copy and new virtual dom copy and create a new virtual dom copy with updated data and only updates dom where nessesary.</p>
-            </article>
-            <article className='blog-container'>
-                <h1>1. What are the difference between props and state?</h1>
+                <h1>3. What are the difference between props and state?</h1>
                 <div className='blog'>
                     <div>
                         <h3>Props</h3>
@@ -24,10 +30,6 @@ const Article = () => {
                         <p>3. State is mutable.</p>
                     </div>
                 </div>
-            </article>
-            <article className='blog-container'>
-                <h1>1. How useState Works?</h1>
-                <p>UseState is a react hook, that contain a variable and a function. At the first time when a component load usestate set the initial value to its variable. When change the state then setState function assign and update the new value to the usestate variable and finally rerender the function.</p>
             </article>
         </div>
     );
